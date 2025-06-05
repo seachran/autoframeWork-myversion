2fa8773599a048a6209511348eae8cce8a93eba5
Yaroslav Draigal <yaroslavdraigal@gmail.com>
2025-06-05 20:43:44 +0300

    task 4 bdd p1.2

--- paraBankForStudents/src/test/java/com/stv/bdd/runners/bddRunner.java
+++ paraBankForStudents/src/test/java/com/stv/bdd/runners/bddRunner.java
@@ -2,10 +2,10 @@ package com.stv.bdd.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-        features = "src/test/java/com/stv/bdd/features",
+        features = "paraBankForStudents/src/test/java/com/stv/bdd/features",
         glue = "com.stv.bdd.steps",
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
 )
-public class BddRunner extends AbstractTestNGCucumberTests {
+public class bddRunner extends AbstractTestNGCucumberTests {
 }
